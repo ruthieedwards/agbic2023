350114f29e2bc2593e90913d4125bed612989dda
ruthieedwards <ruthieproctor@gmail.com>
2023-07-06 22:36:08 -0400

    Added tweens to cards dealt to hand

--- Playspace.gd
+++ Playspace.gd
@@ -19,6 +19,10 @@ var randomCardSelected = []
 @onready var vertRadius = get_viewport().size.y * 0.4
 var startingAngle = deg_to_rad(90) - 0.5 # pi/2
 var ovalAngleVector = Vector2()
+var cardSpread = 0.25
+var cardAngle = 0
+var cardNumber = 0
+var numCardsInHand = 0
 
 # i feel like this shouldn't be here but it is
 enum{
@@ -40,29 +44,52 @@ func _ready():
 #	pass
 
 func drawACard():
+	cardAngle = PI/2 + cardSpread*(float(numCardsInHand)/2 - numCardsInHand)
 	var newCard = cardBase.instantiate()
 	randomCardSelected = randi() % deckSize
 	newCard.cardName = playerHandTemp.cardList[randomCardSelected]
 	
 	#place the card along an oval
-	ovalAngleVector = Vector2(horizRadius * cos(startingAngle), - vertRadius * sin(startingAngle))
-	newCard.startPos = $Deck.position #- newCard.size/2
+	ovalAngleVector = Vector2(horizRadius * cos(cardAngle), - vertRadius * sin(cardAngle))
+	newCard.startPos = $Deck.position - newCard.size/2 # - cardSize/2 
 	newCard.targetPos = centerCardOval + ovalAngleVector - newCard.size/2
 	newCard.startRot = 0
-	newCard.targetRot = (PI/2 - startingAngle)/4 
+	newCard.targetRot = (PI/2 - cardAngle)/4 
+	#newCard.scale *= cardSize/newCard.size #enable if scaling needed
 	newCard.currentCardState = movingDrawnCardToHand
+	
+	cardNumber = 0
+	
+	for card in $Cards.get_children(): # reorganize hand
+		cardAngle = PI/2 + cardSpread*(float(numCardsInHand)/2 - cardNumber) 
+		ovalAngleVector = Vector2(horizRadius * cos(cardAngle), - vertRadius * sin(cardAngle))
+#		card.startPos = card.position
+		card.targetPos = centerCardOval + ovalAngleVector - card.size/2 #- cardSize
+		card.startRot = card.rotation
+		card.targetRot = (PI/2 - cardAngle)/4 
+#		card.targetRot = (90 - rad_to_deg(cardAngle))/4 #doesnt work?
+		
+		cardNumber += 1
+		if card.currentCardState == inHand:
+			card.currentCardState = reorganizingHand
+			card.startPos = card.position
+		elif card.currentCardState == movingDrawnCardToHand:
+			card.startPos = card.targetPos - ((card.targetPos - card.position)/(1-card.t))
+	
+
 	
 	print("deckSize =", deckSize)
 	print("randomCardSelected =", randomCardSelected)
-	
 	
 #	new_card.scale *= cardSize/new_card.size # enable if you need to resize the cards
 	$Cards.add_child(newCard)
 	print("newCard name: ",newCard.cardName)
 #		print("Cards Group Count =", $Cards.get_child_count())
 	playerHandTemp.cardList.erase(playerHandTemp.cardList[randomCardSelected])
-	startingAngle += 0.3
+	cardAngle += 0.3
 	deckSize -= 1
+	numCardsInHand += 1
+#	cardNumber += 1
 	return deckSize
 #	print(playerHandTemp.cardList)
 	
--- Scripts/CardBase.gd
+++ Scripts/CardBase.gd
@@ -13,7 +13,8 @@ var targetPos = 0
 var startRot = 0
 var targetRot = 0
 var t = 0
-@export var drawTime =  .5 #time in seconds to draw card
+@export var drawTime =  .3 #time in seconds to draw card
+var isTweening = false
 
 
 # these were moved to the _ready() function
@@ -44,6 +45,26 @@ func _physics_process(delta):
 		focusedInHand:
 			pass
 		movingDrawnCardToHand: # animate from deck to hand
+			#Tweening in Godot 4.0
+			var tween = create_tween()
+			tween.tween_property(self, "position", targetPos, drawTime).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+			tween.tween_property(self, "rotation", targetRot, drawTime).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+			
+			# movement using lerp instead of tweens
+			if t <= 2: # while the timer is counting down
+#				position = startPos.lerp(targetPos,t) # moves from deck to hand
+##				rotation = startRot * (1-t) + targetRot*t # rotates from deck to hand
+				t += delta/float(drawTime) # essentially a timer
+#
+#
+			else: #if the time goes over by accident
+				position = targetPos
+				rotation = targetRot
+				currentCardState = inHand
+				t = 0
+			#TO DO: flip the card (vid 3 - I skipped this step)
+			
+		reorganizingHand:
 			if t <= 1: # while the timer is counting down
 				position = startPos.lerp(targetPos,t) # moves from deck to hand
 				rotation = startRot * (1-t) + targetRot*t # rotates from deck to hand
@@ -53,9 +74,6 @@ func _physics_process(delta):
 				rotation = targetRot
 				currentCardState = inHand
 				t = 0
-			#TO DO: flip the card (vid 3 - I skipped this step)
-		reorganizingHand:
-			pass
 			
 			
 
