7bb5d6bf851eaf4fe24dbf9a0321f17fccd9b9c5
ruthieedwards <ruthieproctor@gmail.com>
2023-07-20 08:11:42 -0400

    implemented a very shitty level switcher

--- Scenes/SceneChanger.gd
+++ Scenes/SceneChanger.gd
@@ -2,6 +2,8 @@ extends Control
 
 var gameScene = 'res://Scenes/PlaySpace.tscn'
 var titleScreen = 'res://Scenes/TitleScreen.tscn'
+var levelSelect = 'res://Scenes/LevelSelect.tscn'
+@export var levelNumber = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,8 +12,17 @@ func _ready():
 func GoToTitle():
 	get_tree().change_scene_to_file(titleScreen)
 	
+func GoToLevelSelect():
+	get_tree().change_scene_to_file(levelSelect)
+	
 func GoToGame():
 	get_tree().change_scene_to_file(gameScene)
+	
+func ChangeLevel():
+	$"../../LevelText".text = str("Level ",levelNumber)
+#	get_node("../LevelText").text = str("Level ",levelNumber)
+#	$Bars/EnergyText.text = str("Energy: ",currentEnergy)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
